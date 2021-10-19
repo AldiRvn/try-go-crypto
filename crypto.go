00da08cb4c649318a55bf0a7bb2f242dce64e5de
AldiRvn <aldirvn4@gmail.com>
2021-10-19 09:02:41 +0700

    feat: Add configuration to struct for easy customization enc/dec mechanism and with new method to create object with default configuration

--- crypto.go
+++ crypto.go
@@ -4,8 +4,9 @@ import (
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/rand"
-	"crypto/sha256"
+	"crypto/sha512"
 	"encoding/hex"
+	"hash"
 	"io"
 	"log"
 
@@ -14,18 +15,33 @@ import (
 
 type Crypto struct {
 	PassPhrase string
+	IvSize     int
+	//? PBKDF2
+	IterCount int
+	KeyLen    int
+	Hash      func() hash.Hash
+}
+
+func NewWithDefaultConfig(passPhrase string) Crypto {
+	return Crypto{
+		PassPhrase: passPhrase,
+		IvSize:     aes.BlockSize,
+		IterCount:  1000,
+		KeyLen:     32,
+		Hash:       sha512.New,
+	}
 }
 
 func (this Crypto) derivationKey(salt []byte) (key []byte, newSalt []byte) {
 	if salt == nil {
-		salt = make([]byte, aes.BlockSize)
+		salt = make([]byte, this.IvSize)
 		// http://www.ietf.org/rfc/rfc2898.txt
 		if _, err := rand.Read(salt); err != nil {
 			log.Println(err)
 		}
 	}
 
-	key = pbkdf2.Key([]byte(this.PassPhrase), salt, 1000, 32, sha256.New)
+	key = pbkdf2.Key([]byte(this.PassPhrase), salt, this.IterCount, this.KeyLen, this.Hash)
 	newSalt = salt
 	return
 }
@@ -55,18 +71,18 @@ func (this Crypto) encryptBase(data, saltRaw, cipherRaw []byte) (res string, sal
 		return
 	}
 
-	cipherTextNew := make([]byte, aes.BlockSize+len(data))
-	iv := cipherTextNew[:aes.BlockSize]
+	cipherTextNew := make([]byte, this.IvSize+len(data))
+	iv := cipherTextNew[:this.IvSize]
 	if cipherRaw == nil {
 		if _, err := io.ReadFull(rand.Reader, iv); err != nil {
 			log.Println(err)
 			return
 		}
 	} else {
-		copy(iv, cipherRaw[:aes.BlockSize])
+		copy(iv, cipherRaw[:this.IvSize])
 	}
 
-	cipher.NewOFB(block, iv).XORKeyStream(cipherTextNew[aes.BlockSize:], data)
+	cipher.NewOFB(block, iv).XORKeyStream(cipherTextNew[this.IvSize:], data)
 
 	res = hex.EncodeToString(cipherTextNew)
 	saltNew = hex.EncodeToString(salt)
@@ -95,8 +111,8 @@ func (this Crypto) Decrypt(saltRaw string, cipherRaw string) (res string) {
 		log.Println(err)
 	}
 
-	dec := make([]byte, aes.BlockSize+len(cipherRaw))
-	cipher.NewOFB(block, cipherText[:aes.BlockSize]).XORKeyStream(dec, cipherText[aes.BlockSize:])
+	dec := make([]byte, this.IvSize+len(cipherRaw))
+	cipher.NewOFB(block, cipherText[:this.IvSize]).XORKeyStream(dec, cipherText[this.IvSize:])
 
 	res = string(dec)
 	return
--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import "log"
 
 func main() {
-	crypto := Crypto{PassPhrase: "76b8e2c3f7c84de0b83eace56d36ae93"}
+	crypto := NewWithDefaultConfig("76b8e2c3f7c84de0b83eace56d36ae93")
 
 	enc, salt := crypto.Encrypt([]byte("test-ws"))
 	log.Println("Encrypt:\t", enc)
