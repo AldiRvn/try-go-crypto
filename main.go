7b56de379ee3f0e7ac2e31cee719c6bbf2336d3d
AldiRvn <aldirvn4@gmail.com>
2021-10-19 08:48:42 +0700

    fix: Typo variable name crypt

--- main.go
+++ main.go
@@ -3,14 +3,14 @@ package main
 import "log"
 
 func main() {
-	crypt := Crypto{PassPhrase: "76b8e2c3f7c84de0b83eace56d36ae93"}
+	crypto := Crypto{PassPhrase: "76b8e2c3f7c84de0b83eace56d36ae93"}
 
-	enc, salt := crypt.Encrypt([]byte("test-ws"))
+	enc, salt := crypto.Encrypt([]byte("test-ws"))
 	log.Println("Encrypt:\t", enc)
 	log.Println("Salt:\t", salt)
 
-	dec := crypt.Decrypt(salt, enc)
+	dec := crypto.Decrypt(salt, enc)
 	log.Println("Dec:\t", dec)
 
-	log.Println("Compare:\t", crypt.CompareEncrypt(enc, salt, "test-ws"))
+	log.Println("Compare:\t", crypto.CompareEncrypt(enc, salt, "test-ws"))
 }
